b33f89381136daf1d7b5219a4a4a33a4da4a973f
Prince Yadav <167596785+warrior-hub@users.noreply.github.com>
2026-09-05 14:48:06 +0530

    Update API base URL for authFetch

--- frontend/src/authFetch.js
+++ frontend/src/authFetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://social-media-ppvc.onrender.com"
+  baseURL: "https://social-media-duu3.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
